5f5f40374420b76694c158dc2853f88a697bac52
Vitor Savian <vitor.savian@suse.com>
2024-09-12 12:21:02 -0300

    Removed owlbear binary add a interface for the tcp connection

--- elminster/pkg/server/connections/tcp/config.go
+++ elminster/pkg/server/connections/tcp/config.go
@@ -1,1 +1,18 @@
 package tcp
+
+import "github.com/labstack/echo/v4"
+
+type Server struct {
+	IPv4 string
+	Port int
+}
+
+func CreateServer(config Server) *echo.Echo {
+	e := echo.New()
+
+	return e
+}
+
+func Listen(server *echo.Echo) {
+
+}
