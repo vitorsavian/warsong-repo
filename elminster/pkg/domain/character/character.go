fdf4522541ce9b01d907357265ccf8e9f39e6d4a
Vitor Savian <vitor.savian@suse.com>
2024-09-14 09:17:29 -0300

    Add mongo to docker compose and beginning character development

---
+++ elminster/pkg/domain/character/character.go
@@ -0,0 +1,38 @@
+package character
+
+type Character struct {
+	Id   string
+	Name string
+	HP   int16
+	SP   int16
+
+	Str int8
+	Dex int8
+	Con int8
+	Cha int8
+	Int int8
+	Wil int8
+
+	Sanity  int16
+	Courage int16
+
+	Feat []Feat
+}
+
+type Feat struct {
+	Name        string
+	Description string
+}
+
+func (c *Character) LevelUp() {
+	c.HP += int16(c.Con)
+	c.SP += int16(c.Int)
+}
+
+func (c *Character) Fight() {
+
+}
+
+func (c *Character) Rest() {
+
+}
