7deeba14b48447a01abe7fe685c91ce63c1a82fa
Vitor Savian <vitor.savian@suse.com>
2024-09-30 19:48:43 -0300

    Refac: more refactoring in tasha

---
+++ tasha/src/connections/tcp.rs
@@ -0,0 +1,58 @@
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+pub async fn server() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+    println!("Hello, world!");
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+// the input to our `create_user` handler
+#[allow(dead_code)]
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+    email: String,
+    password: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    match payload.username.is_empty() {
+        true => println!("test"),
+        false => println!("fon"),
+    }
+
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
+}
