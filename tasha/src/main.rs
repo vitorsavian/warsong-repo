f15a3ecbbc64d0b5a6f9f53822b707bc1ae20de3
Vitor Savian <vitor.savian@suse.com>
2024-09-30 00:07:27 -0300

    build: Create the server

--- tasha/src/main.rs
+++ tasha/src/main.rs
@@ -10,7 +10,14 @@ use serde::{Deserialize, Serialize};
 async fn main() {
     // initialize tracing
     tracing_subscriber::fmt::init();
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
     println!("Hello, world!");
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
 
 async fn root() -> &'static str {
@@ -18,9 +25,12 @@ async fn root() -> &'static str {
 }
 
 // the input to our `create_user` handler
+#[allow(dead_code)]
 #[derive(Deserialize)]
 struct CreateUser {
     username: String,
+    email: String,
+    password: String,
 }
 
 // the output to our `create_user` handler
@@ -31,6 +41,11 @@ struct User {
 }
 
 async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    match payload.username.is_empty() {
+        true => println!("test"),
+        false => println!("fon"),
+    }
+
     // insert your application logic here
     let user = User {
         id: 1337,
