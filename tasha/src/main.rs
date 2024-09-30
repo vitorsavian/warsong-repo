1fde89f06dffcecf2c2fad1c55afede26567a6dc
Vitor Savian <vitor.savian@suse.com>
2024-09-29 22:41:32 -0300

    feat: Add create user function and add dependencys

--- tasha/src/main.rs
+++ tasha/src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
+use axum::{
+    http::StatusCode,
+    routing::{get, post},
+    Json, Router,
+};
+
+use serde::{Deserialize, Serialize};
+
+#[tokio::main]
+async fn main() {
+    // initialize tracing
+    tracing_subscriber::fmt::init();
     println!("Hello, world!");
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+// the input to our `create_user` handler
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+// the output to our `create_user` handler
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> (StatusCode, Json<User>) {
+    // insert your application logic here
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    // this will be converted into a JSON response
+    // with a status code of `201 Created`
+    (StatusCode::CREATED, Json(user))
 }
