96ffbd85cd00f14c4d8c09fbb86d7832d651539a
Vitor Savian <vitor.savian@suse.com>
2024-10-20 15:11:21 -0300

    Add cli with shell for project

--- cmd.sh
+++ cmd.sh
@@ -1,24 +1,61 @@
 #!/bin/bash
 
 function usage() {
-    echo "Usage: $0 [opção]"
+    echo "Usage: $0 [options]"
     echo "Options:"
     echo "  -h, --help     Show this message"
     echo "  --elminster    Use Elminster"
     echo "  --tasha        Use Tasha"
 }
 
-if [ $# -eq 0 ]; then
-    usage
-    exit 1
-fi
+function usage_elminster() {
+    echo "Usage: $0 --elminster [options]"
+    echo "Optiosn:"
+    echo "  -h, --help     Show this message"
+}
 
-case $1 in
+function handle_elminster() {
+    case $1 in
+    -h|--help)
+        usage_elminster
+        ;;
+    *)
+        usage_elminster
+        exit 1
+        ;;
+    esac
+}
+
+# function usage_tasha() {
+
+# }
+
+# function usage_owlbear() {
+
+# }
+
+# function usage_bigby() {
+
+# }
+
+function init() {
+    if [ $# -eq 0 ]; then
+        usage
+        exit 1
+    fi
+
+    case $1 in
     -h|--help)
         usage
+        ;;
+    --elminster)
+        handle_elminster "$2"
         ;;
     *)
         usage
         exit 1
         ;;
-esac
+    esac
+}
+
+init "$@"
