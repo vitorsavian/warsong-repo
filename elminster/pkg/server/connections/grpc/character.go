ea5c2b85983804c243058e8227c00c8e9d035288
Vitor Savian <vitor.savian@suse.com>
2024-10-19 21:34:16 -0300

    Refac in grpc server

--- elminster/pkg/server/connections/grpc/character.go
+++ elminster/pkg/server/connections/grpc/character.go
@@ -4,35 +4,9 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/sirupsen/logrus"
 	"github.com/vitorsavian/warsong-repo/elminster/pkg/adapter"
 	"github.com/vitorsavian/warsong-repo/elminster/pkg/server/connections/grpc/pb"
-	"github.com/vitorsavian/warsong-repo/elminster/pkg/server/controllers"
 )
-
-type server struct {
-	pb.UnimplementedCharacterServer
-	pb.UnimplementedPingServer
-	pb.UnimplementedWeaponServer
-	characterController *controllers.CharacterController
-}
-
-func NewServer() *server {
-	characterController, err := controllers.GetController()
-	if err != nil {
-		logrus.Fatalf("Failed to get character controller: %v", err)
-	}
-
-	return &server{
-		characterController: characterController,
-	}
-}
-
-func (s *server) Ping(_ context.Context, ping *pb.PingRequest) (*pb.PingResponse, error) {
-	return &pb.PingResponse{
-		Ping: 1,
-	}, nil
-}
 
 func (s *server) CreateCharacter(_ context.Context, character *pb.CreateCharacterRequest) (*pb.CreateCharacterResponse, error) {
 
--- elminster/pkg/server/connections/grpc/config.go
+++ elminster/pkg/server/connections/grpc/config.go
@@ -1,1 +1,33 @@
 package grpc
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+	"github.com/vitorsavian/warsong-repo/elminster/pkg/server/connections/grpc/pb"
+	"github.com/vitorsavian/warsong-repo/elminster/pkg/server/controllers"
+)
+
+type server struct {
+	pb.UnimplementedCharacterServer
+	pb.UnimplementedPingServer
+	pb.UnimplementedWeaponServer
+	characterController *controllers.CharacterController
+}
+
+func NewServer() *server {
+	characterController, err := controllers.GetController()
+	if err != nil {
+		logrus.Fatalf("Failed to get character controller: %v", err)
+	}
+
+	return &server{
+		characterController: characterController,
+	}
+}
+
+func (s *server) Ping(_ context.Context, ping *pb.PingRequest) (*pb.PingResponse, error) {
+	return &pb.PingResponse{
+		Ping: 1,
+	}, nil
+}
